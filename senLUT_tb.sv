5b35c73e043b4115a5417f828bfa144a24219af1
Erick Cordero Rojas <eguicoro2@gmail.com>
2019-04-17 18:24:20 -0600

    LUT update

--- senLUT_tb.sv
+++ senLUT_tb.sv
@@ -1,20 +1,18 @@
-module LUT_tb();
-    logic op_selector, clk;
+module SenLUT_tb();
+    logic clk;
     logic [31:0] angle;
     logic [31:0] value; 
     logic [16:0] contador;
 
-    LUT DUT(op_selector,
-                clk,
+    SenLUT DUT( clk,
                 angle,
                 value);
  
     initial begin
 		// ADD
         clk=1'b0;
-		op_selector = 1;
         angle = 32'b111;
-        contador = 16'b101;
+        contador = 16'b100;
 	end
 	
 	always begin
